75a052779e284601f84a5fe7542539ec9d3731f2
Valerio DAlessio <u105550@canda.com>
2022-11-25 16:19:16 +0100

    Added the StudentView and ViewModel

---
+++ OnTheMap/View/StudentView/StudentView.swift
@@ -0,0 +1,20 @@
+//
+//  StudentView.swift
+//  OnTheMap
+//
+//  Created by Valerio D'ALESSIO on 25/11/22.
+//
+
+import SwiftUI
+
+struct StudentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StudentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StudentView()
+    }
+}
---
+++ OnTheMap/ViewModel/StudentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StudentViewModel.swift
+//  OnTheMap
+//
+//  Created by Valerio D'ALESSIO on 25/11/22.
+//
+
+import Foundation
